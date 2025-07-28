c5d8394f673f59829aa8ea81483cf91932062495
jamesjo <jamescakr@gmail.com>
2025-07-27 21:48:56 -0600

    add missing authController import

--- routes/task.api.js
+++ routes/task.api.js
@@ -1,6 +1,7 @@
 //라우터 정의를 위해 express에서 제공하는 Router를 쓰기위해 불러오는 작업
 const express = require("express");
 const taskController = require("../controller/task.controller");
+const authController = require("../controller/auth.controller");
 const router = express.Router();
 
 //라우터의 경로를 정의하고, 특정 경로로 요청이 들어왔을때 어떻게 처리할지 알려주는 코드
