d378934e3992e2430506917fd30f2cf2bf09fce2
jamesjo <jamescakr@gmail.com>
2025-07-27 22:08:56 -0600

    change app.js for deployment

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ const app = express();
 const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD;
 console.log("MongoURI", MONGODB_URI_PROD);
 
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 app.use(cors());
 app.use("/api", indexRouter);
-const mongoURI = `mongodb://localhost:27017/todo-demo`; //로컬 작업할때 활성화 시키기
-// const mongoURI = MONGODB_URI_PROD; //배포 작업할떄 활성화 시키기
+// const mongoURI = `mongodb://localhost:27017/todo-demo`; //로컬 작업할때 활성화 시키기
+const mongoURI = MONGODB_URI_PROD; //배포 작업할떄 활성화 시키기
 
 //mongoose setting
 mongoose
@@ -24,7 +24,7 @@ mongoose
     console.log("DB connected failed :(", err);
   });
 
-//app listener setting 
+//app listener setting
 //중요!!!! heroku 배포할때 이 부분을 아래 const PORT로 바꿔야함
 // app.listen(4000, () => {
 //   console.log("server on 4000!");
